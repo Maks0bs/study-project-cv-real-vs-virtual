3eb7c985a9a6a96e78d2c655a62150261cdb4107
maks0bs <maksthebro173@gmail.com>
2023-12-16 21:44:21 +0100

    fix percentages

--- common/helpers.py
+++ common/helpers.py
@@ -4,4 +4,4 @@ DETECTION_SCORES_TO_PERCENTAGES_CONST = (np.uint64(0b1) << np.uint64(0b101)).ast
 MAX_PERC = 0.95
 
 def detection_scores_to_percentages(detection_scores):
-    return np.array([min(x + DETECTION_SCORES_TO_PERCENTAGES_CONST, 0.95) for x in list(detection_scores)])
+    return np.array([min(x + DETECTION_SCORES_TO_PERCENTAGES_CONST, MAX_PERC) for x in list(detection_scores)])
